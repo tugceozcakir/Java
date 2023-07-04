1d889d558f55b7639cc967213bf4140f09d6eb76
tugceozcakir <tugceozcakir15@gmail.com>
2023-07-04 17:01:41 +0300

    update

--- AverageCalculator/AverageCalculator/src/App.java
+++ AverageCalculator/AverageCalculator/src/App.java
@@ -28,11 +28,6 @@ public class App {
     double average = (double) result / 6;
 
     System.out.println("Average: " + average);
-
-    if (average > 60) {
-        System.out.println("Passed the class.");
-    } else {
-        System.out.println("Failed in class.");
-    }
+    System.out.println(average >= 60 ? " Sınıfı Geçti" : "Sınıfta Kaldı");
 }
 }
