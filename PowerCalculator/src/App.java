cb13c62fc84524c146294b48aa9a61c530c120a0
tugceozcakir <tugceozcakir15@gmail.com>
2023-07-06 18:23:31 +0300

    update

---
+++ PowerCalculator/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        //Ask for the base
+        System.out.print("Enter the base: ");
+        int base = scanner.nextInt();
+
+        //Ask for the exponent
+        System.out.print("Enter the power: ");
+        int power = scanner.nextInt();
+
+        int result = 1;
+        for (int i = 0; i < power; i++) {
+            result *= base;
+        }
+
+        System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+}
