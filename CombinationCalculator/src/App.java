e7734e32bdbca5e08276e415798955aba7c7cc40
tugceozcakir <tugceozcakir15@gmail.com>
2023-07-07 18:25:07 +0300

    update

--- CombinationCalculator/src/App.java
+++ CombinationCalculator/src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-         Scanner input=new Scanner(System.in);
+        Scanner input=new Scanner(System.in);
         int n,r;
         int total1=1;
         int total2=1;
