62e971ddf3e7035b269fe0f0f8ce040ffd39efdb
tugceozcakir <tugceozcakir15@gmail.com>
2023-07-15 01:29:00 +0300

    update

---
+++ MinesweeperGame/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Minesweeper mineSweeper = new Minesweeper(4,4);
+        mineSweeper.run();
+    }
+}
---
+++ MinesweeperGame/src/Minesweeper.java
@@ -0,0 +1,137 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Minesweeper {
+    int rowNumber;
+    int colNumber;
+    String[][] mayinMap;
+    String[][] gameMap;
+    int mayinSayisi;
+    int a;
+    int b;
+    int count;
+    boolean isTrue = true;
+
+
+    public Minesweeper(int row, int col) {
+        this.rowNumber = row;
+        this.colNumber = col;
+        this.gameMap = new String[row][col];
+        this.mayinMap = new String[row][col];
+        this.mayinSayisi = (row * col) / 4;
+
+    }
+
+    public void mayinMap() {
+        for (int i = 0; i < rowNumber; i++) {
+            for (int j = 0; j < colNumber; j++) {
+                mayinMap[i][j] = "-";
+                gameMap[i][j] = "-";
+
+            }
+        }
+    }
+
+    public void randomNumber() {
+        Random r = new Random();
+        for (int i = 0; i < this.mayinSayisi; i++) {
+            int rsayi = r.nextInt(this.rowNumber);
+            int rsayi2 = r.nextInt(this.colNumber);
+            if (!this.mayinMap[rsayi][rsayi2].equals("*")) {
+                this.mayinMap[rsayi][rsayi2] = "*";
+            }
+        }
+    }
+
+    public void printmayinMap() {
+        System.out.println("Mayinlarin Konumu");
+        randomNumber();
+        for (int i = 0; i < this.rowNumber; i++) {
+            for (int j = 0; j < this.colNumber; j++) {
+                if (!this.mayinMap[i][j].equals("*")) {
+                    this.mayinMap[i][j] = "-";
+                }
+                System.out.print(this.mayinMap[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("==================================");
+        System.out.println("Mayın Tarlası Oyununa Hoş Geldiniz !");
+    }
+
+    public void printGameMap() {
+        for (int i = 0; i < this.rowNumber; i++) {
+            for (int j = 0; j < this.colNumber; j++) {
+                this.gameMap[i][j] = "-";
+                System.out.print(this.gameMap[i][j] + " ");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public void indisSecme() {
+        Scanner input = new Scanner(System.in);
+        isTrue = false;
+        while (!isTrue) {
+            System.out.print("Satir Giriniz : ");
+            a = input.nextInt();
+            System.out.print("Sutun Giriniz : ");
+            b = input.nextInt();
+            if (a > rowNumber || b > colNumber) {
+                System.out.println("Map sınırları dışında seçim yaptınız, lütfen tekrar giriniz !");
+                continue;
+            }
+            if (mayinMap[a][b].equals("*")) {
+                System.out.println("Game Over!");
+                break;
+
+            }
+            control();
+            if (finish()) {
+
+                System.out.println("Tebrikler kazandınız :)))))");
+                break;
+            }
+        }
+    }
+
+    public void control() {
+        count = 0;
+        for (int i = (a - 1); i <= (a + 1); i++) {
+            for (int j = (b - 1); j <= (b + 1); j++) {
+                if (i < 0 || j < 0 || i >= this.rowNumber || j >= this.colNumber) {
+                    continue;
+                }
+                if (this.mayinMap[i][j].equals("*")) {
+                    count++;
+                }
+            }
+        }
+
+        this.gameMap[a][b] = String.valueOf(count);
+        this.mayinMap[a][b] = String.valueOf(count);
+        for (int i = 0; i < this.rowNumber; i++) {
+            for (int j = 0; j < this.colNumber; j++) {
+                System.out.print(this.gameMap[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public boolean finish() {
+        for (int i = 0; i < this.rowNumber; i++) {
+            for (int j = 0; j < this.colNumber; j++) {
+                if (this.mayinMap[i][j].equals("-")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public void run() {
+        mayinMap();
+        printmayinMap();
+        indisSecme();
+    }
+}
