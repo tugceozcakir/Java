96ff00e9afd54a3d64d9f1d9b10baec3ad846ea4
tugceozcakir <tugceozcakir15@gmail.com>
2023-07-14 18:41:38 +0300

    update

---
+++ BoxingMatchGame/src/App.java
@@ -0,0 +1,11 @@
+import model.Fighter;
+import model.Ring;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Fighter marc = new Fighter("Marc" , 15 , 100, 90, 0);
+        Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
+        Ring r = new Ring(marc,alex , 90 , 100);
+        r.run();
+    }
+}
