e811103025942b1d4cdfb5b55210b61d0c9fe7a2
tugceozcakir <tugceozcakir15@gmail.com>
2023-07-25 20:02:52 +0300

    update

---
+++ ThreadRace/src/NumberProcessor.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+class NumberProcessor extends Thread {
+    private List<Integer> numbers;
+    private List<Integer> evenNumbers;
+    private List<Integer> oddNumbers;
+
+    public NumberProcessor(List<Integer> numbers, List<Integer> evenNumbers, List<Integer> oddNumbers) {
+        this.numbers = numbers;
+        this.evenNumbers = evenNumbers;
+        this.oddNumbers = oddNumbers;
+    }
+
+    @Override
+    public void run() {
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                synchronized (evenNumbers) {
+                    evenNumbers.add(number);
+                }
+            } else {
+                synchronized (oddNumbers) {
+                    oddNumbers.add(number);
+                }
+            }
+        }
+    }
+}
